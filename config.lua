0eee0e945d1d4345fc478c413c4f1a666910c9de
RussellJW <76913433+RussellJW@users.noreply.github.com>
2021-06-27 17:11:00 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 
 Config = {}
 
-Config.Webhook = 'https://discord.com/api/webhooks/850877273195675698/ynJQaEJmJQmNl1dGkq1EDrfSfYXIUYZ6x-FrR9BpEfUnB73px0i8GP0BBEDI7ZC7QF-T'
+Config.Webhook = 'INSERT_WEBHOOK_HERE'
 
 Config.Color = "0"
-Config.Tit = "IP = 135.125.203.3"
-Config.Desc = "Teamspeak - 135.125.203.3"
+Config.Tit = "IP = YOUR_IP"
+Config.Desc = "Teamspeak - YOUR_IP"
 
 Config.Name = "Server Status"
-Config.Avatar = "https://i.imgur.com/uQ49Spj.png"
-Config.Message = "**@here Server je nyni ONLINE! Uzivejte roleplay. <3**"
+Config.Avatar = "IMGUR_IMAGE"
+Config.Message = "**@here YOUR_MESSAGE <3**"
